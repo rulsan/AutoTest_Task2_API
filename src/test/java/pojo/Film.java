cde977a222ce7fa8eb5fede5f7ac3134b206dd86
ruslan <borisenko-ruslan@mail.ru>
2022-08-07 02:32:14 +0300

    POJOs were refactored

--- src/test/java/pojo/Film.java
+++ src/test/java/pojo/Film.java
@@ -1,6 +1,8 @@
 package pojo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
@@ -11,6 +13,16 @@ public class Film {
     private List<String> charactersHTTP;
     private String url;
 
+    public Film() {}
+
+    @JsonCreator
+    public Film(@JsonProperty("title") String title, @JsonProperty("characters") List<String> charactersHTTP, @JsonProperty("url") String url) {
+        this.title = title;
+        this.charactersHTTP = charactersHTTP;
+        this.url = url;
+    }
+
+    @JsonProperty("title")
     public String getTitle() {
         return title;
     }
@@ -19,6 +31,7 @@ public class Film {
         this.title = title;
     }
 
+    @JsonProperty("characters")
     public List<String> getCharacters() {
         return charactersHTTP;
     }
@@ -27,11 +40,36 @@ public class Film {
         this.charactersHTTP = characters;
     }
 
+    @JsonProperty("url")
     public String getUrl() {
         return url;
     }
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "The Film is '" + title + '\'' + ", charactersHTTP=" + this.getCharacters().toString() + ", url='" + url + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return title.equals(film.title) && charactersHTTP.equals(film.charactersHTTP) && url.equals(film.url);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + ((this.getTitle() == null ? 0 : this.getTitle().hashCode()));
+        result = prime * result + ((this.getCharacters().isEmpty() ? 0 : this.getCharacters().hashCode()));
+        result = prime * result + ((this.getUrl() == null ? 0 : this.getUrl().hashCode()));
+        return result;
     }
 }
--- src/test/java/pojo/Pilot.java
+++ src/test/java/pojo/Pilot.java
@@ -1,6 +1,8 @@
 package pojo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
@@ -10,6 +12,16 @@ public class Pilot {
     private List<String> starshipsHTTP;
     private String url;
 
+    public Pilot() {}
+
+    @JsonCreator
+    public Pilot(@JsonProperty("name") String name, @JsonProperty("starships") List<String> starshipsHTTP, @JsonProperty("url") String url) {
+        this.name = name;
+        this.starshipsHTTP = starshipsHTTP;
+        this.url = url;
+    }
+
+    @JsonProperty("name")
     public String getName() {
         return name;
     }
@@ -18,6 +30,7 @@ public class Pilot {
         this.name = name;
     }
 
+    @JsonProperty("starships")
     public List<String> getStarships() {
         return starshipsHTTP;
     }
@@ -26,11 +39,36 @@ public class Pilot {
         this.starshipsHTTP = starships;
     }
 
+    @JsonProperty("url")
     public String getUrl() {
         return url;
     }
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "The Pilot is '" + name + '\'' + ", starshipsHTTP=" + this.getStarships().toString() + ", url='" + url + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pilot pilot = (Pilot) o;
+        return name.equals(pilot.name) && starshipsHTTP.equals(pilot.starshipsHTTP) && url.equals(pilot.url);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + ((this.getName() == null ? 0 : this.getName().hashCode()));
+        result = prime * result + ((this.getStarships().isEmpty() ? 0 : this.getStarships().hashCode()));
+        result = prime * result + ((this.getUrl() == null ? 0 : this.getUrl().hashCode()));
+        return result;
     }
 }
