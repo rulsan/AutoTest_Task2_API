3840c7e51c88d0cfa230d2ce07aebdc3ba923b46
ruslan <borisenko-ruslan@mail.ru>
2022-08-09 14:52:33 +0300

    The methods were refactored

--- src/test/java/rest/RestTest.java
+++ src/test/java/rest/RestTest.java
@@ -1,53 +1,34 @@
 package rest;
 
-import io.restassured.http.ContentType;
-import io.restassured.response.ExtractableResponse;
+import actions.Actions;
 import org.junit.jupiter.api.Test;
-import pojo.Pilot;
-import pojo.Film;
 import static org.hamcrest.Matchers.*;
-import java.util.List;
-
 import static io.restassured.RestAssured.given;
 
 public class RestTest {
 
-    public ExtractableResponse getObj(String http){
-        return given().when().get(http).then().contentType(ContentType.JSON).extract();
-    }
-
-    public List<Film> getFilms(){
-        return getObj("https://swapi.dev/api/films").jsonPath().getList("results", Film.class);
-    }
-
-    public List<Pilot> getPilots(){
-        return getObj("https://swapi.dev/api/people").jsonPath().getList("results", Pilot.class);
-    }
-
-    public Pilot getPilot(String http){
-        return getObj(http).as(Pilot.class);
-    }
+    public String FirstPilot = "Luke Skywalker";
+    public String SecondPilot = "Biggs Darklighter";
+    public String Film = "A New Hope";
+    public String StarshipClass = "Starfighter";
 
     @Test
     public void RestTest(){
 
-        String LukeSkywalkerPilot = getPilots().stream()
-                .filter(f -> f.getName().equals("Luke Skywalker"))
-                .map(p -> p.getUrl()).findFirst().get();
+        String LukeSkywalkerPilot = Actions.getPilotFromList(FirstPilot);
+        System.out.println(FirstPilot + " is " + LukeSkywalkerPilot);
 
-        System.out.println("Luke Skywalker is " + LukeSkywalkerPilot);
-
-        String XWingShipHTTP =  getFilms().stream()
-                .filter(f -> f.getTitle().equals("A New Hope")).findFirst().get()
+        String XWingShipHTTP =  Actions.getFilms().stream()
+                .filter(f -> f.getTitle().equals(Film)).findFirst().get()
                 .getCharacters().stream()
-                .filter(p -> getPilot(p).getName().equals("Biggs Darklighter"))
-                .map(p -> getPilot(p).getStarships().get(0)).findFirst().get();
+                .filter(p -> Actions.getPilot(p).getName().equals(SecondPilot))
+                .map(p -> Actions.getPilot(p).getStarships().get(0)).findFirst().get();
 
-        System.out.println("Starship is " + XWingShipHTTP);
+        System.out.println("In '" + Film + "' film the pilot '" + SecondPilot + "' piloted " + XWingShipHTTP + " starship");
 
         given().when().get(XWingShipHTTP).then()
                 .assertThat()
-                .body("starship_class", is("Starfighter"))
+                .body("starship_class", is(StarshipClass))
                 .body("pilots", hasItem(LukeSkywalkerPilot));
     }
 }
